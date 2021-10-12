e4a65ada20428562a2343c0c158cfb7d61c90e74
Himank Barve <hbarve1592@gmail.com>
2021-10-12 07:34:12 +0530

    echarts/line/log-axis

---
+++ pages/echarts/line/log-axis.js
@@ -0,0 +1,78 @@
+/** @format */
+
+import ReactECharts from "../../../components/echarts/ReactECharts";
+
+const option = {
+  title: {
+    text: "Log Axis",
+    left: "center",
+  },
+  tooltip: {
+    trigger: "item",
+    formatter: "{a} <br/>{b} : {c}",
+  },
+  legend: {
+    left: "left",
+  },
+  xAxis: {
+    type: "category",
+    name: "x",
+    splitLine: { show: false },
+    data: ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+  },
+  grid: {
+    left: "3%",
+    right: "4%",
+    bottom: "3%",
+    containLabel: true,
+  },
+  yAxis: {
+    type: "log",
+    name: "y",
+    minorSplitLine: {
+      show: true,
+    },
+  },
+  series: [
+    {
+      name: "Log2",
+      type: "line",
+      data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669],
+    },
+    {
+      name: "Log3",
+      type: "line",
+      data: [1, 2, 4, 8, 16, 32, 64, 128, 256],
+    },
+    {
+      name: "Log1/2",
+      type: "line",
+      data: [
+        1 / 2,
+        1 / 4,
+        1 / 8,
+        1 / 16,
+        1 / 32,
+        1 / 64,
+        1 / 128,
+        1 / 256,
+        1 / 512,
+      ],
+    },
+  ],
+};
+
+const Echarts = () => {
+  return (
+    <div>
+      <ReactECharts
+        option={option}
+        style={{
+          height: "500px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Echarts;
