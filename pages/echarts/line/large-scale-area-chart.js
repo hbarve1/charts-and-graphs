bdcb587483d95648e5e5fbbf2d1e840177d1cd04
Himank Barve <hbarve1592@gmail.com>
2021-10-12 07:30:33 +0530

    echarts/line/large-scale-area-chart

---
+++ pages/echarts/line/large-scale-area-chart.js
@@ -0,0 +1,96 @@
+/** @format */
+// https://dev.to/maneetgoyal/using-apache-echarts-with-react-and-typescript-353k
+
+import * as echarts from "echarts";
+
+import ReactECharts from "../../../components/echarts/ReactECharts";
+
+let base = +new Date(1968, 9, 3);
+let oneDay = 24 * 3600 * 1000;
+let date = [];
+let data = [Math.random() * 300];
+for (let i = 1; i < 20000; i++) {
+  var now = new Date((base += oneDay));
+  date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join("/"));
+  data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
+}
+const option = {
+  tooltip: {
+    trigger: "axis",
+    position: function (pt) {
+      return [pt[0], "10%"];
+    },
+  },
+  title: {
+    left: "center",
+    text: "Large Area Chart",
+  },
+  toolbox: {
+    feature: {
+      dataZoom: {
+        yAxisIndex: "none",
+      },
+      restore: {},
+      saveAsImage: {},
+    },
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: date,
+  },
+  yAxis: {
+    type: "value",
+    boundaryGap: [0, "100%"],
+  },
+  dataZoom: [
+    {
+      type: "inside",
+      start: 0,
+      end: 10,
+    },
+    {
+      start: 0,
+      end: 10,
+    },
+  ],
+  series: [
+    {
+      name: "Fake Data",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
+      itemStyle: {
+        color: "rgb(255, 70, 131)",
+      },
+      areaStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: "rgb(255, 158, 68)",
+          },
+          {
+            offset: 1,
+            color: "rgb(255, 70, 131)",
+          },
+        ]),
+      },
+      data: data,
+    },
+  ],
+};
+
+const Echarts = () => {
+  return (
+    <div>
+      <ReactECharts
+        option={option}
+        style={{
+          height: "500px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Echarts;
