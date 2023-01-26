4eac138b2d1b9bc6052028b3e68b2bcd7002a20b
Himank Barve <hbarve1592@gmail.com>
2023-01-26 12:51:16 +0530

    charts database is added

---
+++ data/database.js
@@ -0,0 +1,25 @@
+export const database = {
+  "recharts": {
+    "line-charts": [
+      {title: "simple-line-chart", value: 0},
+      {title: "tiny-line-chart", value: 0},
+      {title: "dashed-line-chart", value: 0},
+      {title: "vertical-line-chart", value: 0},
+      {title: "biaxial-line-chart", value: 0},
+      {title: "vertical-line-chart-with-specified-domain", value: 0},
+      {title: "line-chart-connect-nulls", value: 0},
+      {title: "line-chart-with-xaxis-pading", value: 1},
+      {title: "line-chart-with-reference-lines", value: 0},
+      {title: "customized-dot-line-chart", value: 0},
+      {title: "customized-label-line-chart", value: 0},
+      {title: "synchronised-line-chart", value: 0},
+      {title: "highlinght-and-zoom-line-chart", value: 0},
+      {title: "line-chart-has-multi-series", value: 0},
+      {title: "line-chart-axis-interval", value: 0},
+    ]
+  },
+  "chartjs": {},
+  "apexcharts": {},
+  "echarts": {},
+  "d3": {},
+}
